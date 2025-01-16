750823381cbfd458dfa042a61749a7f313db513a
davidwang758 <davidwang@calicolabs.com>
2025-01-15 17:20:49 -0800

    first commit

--- push_git.sh
+++ push_git.sh
@@ -4,5 +4,5 @@ cur_dir=$(pwd)
 cd /home/davidwang/hackweek2025/hubDirectory
 git add .
 git commit -m "."
-git push https://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:@github.com/davidwang758/trackhub/ main
+#have to remove the push because it contains my access token
 cd ${cur_dir}
